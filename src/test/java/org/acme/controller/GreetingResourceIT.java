0266fa4dc78256b6ca091d14bb3571b6bd47b1e4
Shirin <90757378+juandelsantos@users.noreply.github.com>
2024-02-20 14:08:55 +0100

    suppression test inutile

--- src/test/java/org/acme/controller/GreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme.controller;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-import org.acme.service.GreetingResourceTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
