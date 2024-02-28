701b832944fe7b2e501f0beaa388fc3bfb9f1455
IC17 <Imranchk17@gmail.com>
2024-02-28 12:06:17 +0100

    Clean : Réorganisation du model Images

--- src/main/java/org/acme/model/Images.java
+++ src/main/java/org/acme/model/Images.java
@@ -1,22 +1,21 @@
 package org.acme.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.ws.rs.GET;
 
 public class Images extends PanacheEntityBase {
 
     private int id_image;
+    private String fichier;
+    private String legende;
 
     public int getId_image() {
-
         return id_image;
     }
 
     public void setId_image(int id_image) {
         this.id_image = id_image;
     }
-
-
-    private String fichier;
 
     public String getFichier() {
         return fichier;
@@ -25,8 +24,6 @@ public class Images extends PanacheEntityBase {
     public void setFichier(String fichier) {
         this.fichier = fichier;
     }
-
-    private String legende;
 
     public String getLegende() {
         return legende;
