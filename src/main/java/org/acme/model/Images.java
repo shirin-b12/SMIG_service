8e7b31412d27504cc27bb524306ecfcf8731d2b3
IC17 <Imranchk17@gmail.com>
2024-02-28 11:49:07 +0100

    Rajout du model Images

--- src/main/java/org/acme/model/Images.java
+++ src/main/java/org/acme/model/Images.java
@@ -1,4 +1,38 @@
 package org.acme.model;
 
-public class Images {
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+public class Images extends PanacheEntityBase {
+
+    private int id_image;
+
+    public int getId_image() {
+
+        return id_image;
+    }
+
+    public void setId_image(int id_image) {
+        this.id_image = id_image;
+    }
+
+
+    private String fichier;
+
+    public String getFichier() {
+        return fichier;
+    }
+
+    public void setFichier(String fichier) {
+        this.fichier = fichier;
+    }
+
+    private String legende;
+
+    public String getLegende() {
+        return legende;
+    }
+
+    public void setLegende(String legende) {
+        this.legende = legende;
+    }
 }
