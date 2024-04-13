c1fd8f9c0f8178fc9acdb2b8b554b26a7c2f214a
Shirin <90757378+juandelsantos@users.noreply.github.com>
2024-04-13 16:25:39 +0200

    correction ressource repo

--- src/main/java/org/acme/repository/RessourcesRepository.java
+++ src/main/java/org/acme/repository/RessourcesRepository.java
@@ -20,9 +20,6 @@ public class RessourcesRepository {
     public void persist(Ressources ressource) {
         Ressources.persist(ressource);
     }
-    public Ressources findById(int id) {
-        return Ressources.findById(id);
-    }
 
     public Ressources update(int id, Ressources nouvelleRessource) {
         Ressources ressourceExistante = em.find(Ressources.class, id);
