31619d48637610457234678dda2b768380d7a025
Maxence Guenin <maxence@dataraxys.com>
2024-04-26 11:31:05 +0200

    fix : token delay

--- src/main/java/org/acme/service/TokenService.java
+++ src/main/java/org/acme/service/TokenService.java
@@ -4,8 +4,10 @@ import io.smallrye.jwt.build.Jwt;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.acme.model.Utilisateurs;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @ApplicationScoped
@@ -14,11 +16,13 @@ public class TokenService {
 
     public String generateToken(Utilisateurs user) {
         String role = user.getRole().getNom_role();
-
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + TimeUnit.DAYS.toMillis(1));
         if (user != null) {
             return Jwt.issuer("https://localhost:8081/")
                     .upn(String.valueOf(user.getId_utilisateur()))
                     .groups(new HashSet<>(List.of(role)))
+                    .expiresAt(expiryDate.toInstant())
                     .sign();
         } else {
            return null;
