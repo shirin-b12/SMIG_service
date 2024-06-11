732bb59b275fc44d9b13fd32431884a42ffdfe9a
Imran C. <Imranchk17@gmail.com>
2024-06-11 21:37:18 +0200

    🚧 Test : En cours de correction d'erreur, pas fini teste "TagControllerIT".

--- src/test/java/org/acme/controller/TagControllerIT.java
+++ src/test/java/org/acme/controller/TagControllerIT.java
@@ -3,6 +3,8 @@ package org.acme.controller;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import jakarta.ws.rs.core.MediaType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,14 +19,25 @@ public class TagControllerIT {
     @BeforeEach
     public void setUp() {
         //RestAssured.baseURI = "http://localhost:8081"; // or the actual base URL if different
+        loginAndRetrieveToken();
     }
 
+    private void loginAndRetrieveToken() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body("{\"email\":\"test\", \"mot_de_passe\":\"test\"}")
+                .when().post("http://localhost:8081/utilisateur/login")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        token = response.jsonPath().getString("accessToken");
+    }
 
     @Test
     public void testGetTags() {
         given()
                 .contentType(ContentType.JSON)
-                .body("{\"id_tag\":\"1\"}")
                 .when().get("http://localhost:8081/tags")
                 .then()
                 .statusCode(200);
@@ -34,11 +47,21 @@ public class TagControllerIT {
     public void testCreateTag() {
         given()
                 .contentType(ContentType.JSON)
-                .body("{\"email\":\"test\", \"mot_de_passe\":\"test\"}")
-                .body("{\"nom_tag\":\"cheveux\"}")
+                .header("Authorization", "Bearer " + token) // Utiliser le jeton récupéré
+                .body("{\"nom_tag\":\"tag\"}")
                 .when().post("http://localhost:8081/tags")
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    public void testCreateTagFailed() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"NomTags\":\"tag\"}")
+                .when().post("http://localhost:8081/tags")
+                .then()
+                .statusCode(400);
     }
 
 }
