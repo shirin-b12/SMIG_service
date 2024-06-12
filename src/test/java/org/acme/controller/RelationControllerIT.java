ea2753e89c20bfe2436be88ffda815992955d14f
GuiguiLeGolem <guillaume.mansutti@outlook.com>
2024-06-12 18:01:04 +0200

    C'est lé CaCa :D
    TypesRelationControllerIT est complétement opérationnel. Je m'attaque au RelationControllerIT

--- src/test/java/org/acme/controller/RelationControllerIT.java
+++ src/test/java/org/acme/controller/RelationControllerIT.java
@@ -4,30 +4,46 @@ import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import jakarta.transaction.Transactional;
 import org.acme.model.Relations;
+import org.acme.request.RelationRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import jakarta.transaction.Transactional;
+import io.restassured.RestAssured;
+import org.mockito.Mock;
+
+import java.util.Collections;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 @QuarkusTest
 @TestHTTPEndpoint(RelationController.class)
 public class RelationControllerIT {
 
+    @Mock
+    private Relations mockRelations;
+
+    @Mock
+    private  RelationRequest mockRelationRequest;
+    private int idTest = 1;
+    private int userIdTest = 21;
     private String token;
 
     @BeforeEach
     public void setUp() {
-        //RestAssured.baseURI = "http://localhost:8081"; // or the actual base URL if different
+
+        mockRelations = new Relations();
+        mockRelationRequest = new RelationRequest(21, 21, idTest);
+        // Initialize mockRelations with necessary data
         loginAndRetrieveToken();
     }
 
     private void loginAndRetrieveToken() {
         Response response = given()
                 .contentType(ContentType.JSON)
-                .body("{\"email\":\"test\", \"mot_de_passe\":\"test\"}")
+                .body("{\"email\":\"shirin@laqueen.com\", \"mot_de_passe\":\"123\"}")
                 .when().post("http://localhost:8081/utilisateur/login")
                 .then()
                 .statusCode(200)
@@ -37,36 +53,106 @@ public class RelationControllerIT {
     }
 
     @Test
-    @Transactional
-    public void testCreateRelation() {
-        // Mock a relation object
-        Relations relation = new Relations();
-        relation.setIdRelation(9999); // Set any necessary fields
-
-        // Send a POST request to create the relation
-        given()
+    public void testAddRelation() {
+        Response response = given()
                 .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(relation)
-                .when()
-                .post()
+                .body(mockRelationRequest)
+                .when().post()
                 .then()
-                .statusCode(201); // Assuming 201 Created status code for successful creation
-        // You can add more assertions if needed
+                .statusCode(201)
+                .body("id", is(notNullValue()))
+                .extract().response();
+
+        // Uncomment for debugging purposes
+        System.out.println("\n " + response.getBody().print() + " \n");
     }
 
     @Test
-    public void testGetRelationById() {
-        // Assuming the endpoint "/{id}" retrieves a relation by its ID
-        given()
-                .when()
-                .header("Authorization", "Bearer " + token)
-                .get("/{id}", 1) // Assuming ID 1 exists
+    public void testGetAllRelations() {
+        Response response = given()
+                .when().get()
                 .then()
                 .statusCode(200)
-                .body("id_relation", equalTo(1)); // Adjust the expected response body according to your implementation
+                .contentType(ContentType.JSON)
+                .extract().response();
+
+        // Uncomment for debugging purposes
+        System.out.println("\n " + response.getBody().print() + " \n");
     }
 
-    // Add similar tests for other CRUD operations like update and delete
+    @Test
+    public void testGetRelation() {
+        Response response = given()
+                .pathParam("id", idTest)
+                .when().get("/{id}")
+                .then()
+                .statusCode(200)
+                .extract().response();
 
+        // Uncomment for debugging purposes
+        System.out.println("\n " + response.getBody().print() + " \n");
+    }
+
+    @Test
+    public void testUpdateRelation() {
+        Relations updatedRelations = new Relations();
+        // Initialize updatedRelations with necessary data
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(updatedRelations)
+                .pathParam("id", idTest)
+                .when().put("/{id}")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        // Uncomment for debugging purposes
+        System.out.println("\n " + response.jsonPath().get() + " \n");
+    }
+
+    @Test
+    public void testDeleteRelation() {
+        Response response = given()
+                .pathParam("id", idTest)
+                .when().delete("/{id}")
+                .then()
+                .statusCode(200)
+                .body(is("Relations deleted successfully"))
+                .extract().response();
+
+        // Uncomment for debugging purposes
+        System.out.println("\n " + response.getBody().print() + " \n");
+    }
+
+    @Test
+    public void testGetRelationsByUserId() {
+        Response response = given()
+                .pathParam("userId", userIdTest)
+                .when().get("/user/{userId}")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .extract().response();
+
+        // Uncomment for debugging purposes
+        System.out.println("\n " + response.getBody().print() + " \n");
+    }
+
+    @Test
+    public void testCheckRelationExists() {
+        int userId1 = 1111;
+        int userId2 = 2222;
+
+        Response response = given()
+                .pathParam("userId1", userId1)
+                .pathParam("userId2", userId2)
+                .when().get("/exists/{userId1}/{userId2}")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        // Uncomment for debugging purposes
+        System.out.println("\n " + response.getBody().print() + " \n");
+    }
 }
--- src/test/java/org/acme/controller/TypesRelationControllerIT.java
+++ src/test/java/org/acme/controller/TypesRelationControllerIT.java
@@ -9,27 +9,36 @@ import org.acme.model.TypesRelation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
+import org.mockito.Mock;
 
 import java.io.Console;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.ArgumentMatchers.anyInt;
 
 @QuarkusTest
 @TestHTTPEndpoint(TypesRelationController.class)
 public class TypesRelationControllerIT {
 
+    @Mock
+    private TypesRelation mockTypesRelation;
     private int idTest = 9999;
 
     private String token;
 
     @BeforeEach
     public void setUp() {
+        mockTypesRelation = new TypesRelation();
+
+        mockTypesRelation.setIntitule("Test Intitule");
+
         loginAndRetrieveToken();
     }
 
     private void loginAndRetrieveToken() {
+
         Response response = given()
                 .contentType(ContentType.JSON)
                 .body("{\"email\":\"test\", \"mot_de_passe\":\"test\"}")
@@ -42,71 +51,73 @@ public class TypesRelationControllerIT {
     }
 
     @Test
-    @Transactional
     public void testAddTypesRelation(){
-        TypesRelation typesRelation = new TypesRelation();
-        typesRelation.setIntitule("Test Intitule");
 
-        System.out.println("\n " + RestAssured.baseURI + " \n");
-
-        given()
+        Response response = given()
                 .contentType(ContentType.JSON)
-                .body(typesRelation)
+                .body(mockTypesRelation)
                 .when().post()
                 .then()
                 .statusCode(201)
-                .body("intitule", is("Test Intitule"));
+                .body("idTypeRelation", is(notNullValue()))
+                .body("intitule", is("Test Intitule"))
+                .extract().response();
+
+        //System.out.println("\n " + response.getBody().print() + " \n");
     }
 
     @Test
     public void testGetAllTypesRelations() {
-        given()
+        Response response = given()
                 .when().get()
                 .then()
                 .statusCode(200)
-                .contentType(ContentType.JSON);
+                .contentType(ContentType.JSON)
+                .extract().response();
 
-        System.out.println("\n " + RestAssured.baseURI + " \n");
+        //System.out.println("\n " + response.getBody().print() + " \n");
     }
 
     @Test
     public void testGetTypesRelation() {
-        given()
+
+        Response response = given()
                 .pathParam("id", idTest)
                 .when().get("/{id}")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .extract().response();
 
-
-        System.out.println("\n " + RestAssured.baseURI + " \n");
+        //System.out.println("\n " + response.getBody().print() + " \n");
     }
 
     @Test
     public void testUpdateTypesRelation() {
         TypesRelation tr = new TypesRelation();
         tr.setIntitule("Updated Relation");
-        System.out.println("\n " + RestAssured.baseURI + " \n");
-        given()
+
+        Response response = given()
                 .contentType(ContentType.JSON)
                 .body(tr)
                 .when().put("/999999")
                 .then()
-                .statusCode(200);  // Vérifiez que le code de statut est 200
+                .statusCode(200)
+                .extract().response();  // Vérifiez que le code de statut est 200
 
+        //System.out.println("\n " + response.jsonPath().get() + " \n");
     }
 
     @Test
-    @Transactional
     public void testDeleteTypesRelation(){
-        given()
+        Response response = given()
                 .pathParam("id", idTest)
                 .when().delete("/{id}")
                 .then()
                 .statusCode(200)
-                .body(is("TypesRelation deleted successfully"));
+                .body(is("TypesRelation deleted successfully"))
+                .extract().response();
 
-
-        System.out.println("\n " + RestAssured.baseURI + " \n");
+        //System.out.println("\n " + response.getBody().print() + " \n");
     }
 }
 
