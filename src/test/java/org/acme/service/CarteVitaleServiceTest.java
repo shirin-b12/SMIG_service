0446bc7f9d4b9242df8ce9f1a3cd4fb35ea0be75
Imran C. <Imranchk17@gmail.com>
2024-06-16 21:16:12 +0200

    🐛 test : Fini le test de "CarteVitaleServiceTest.java". Fonctionne normalement, à checker quand même voir si mon test services correspond bien pour le service.

--- src/test/java/org/acme/service/CarteVitaleServiceTest.java
+++ src/test/java/org/acme/service/CarteVitaleServiceTest.java
@@ -1,4 +1,41 @@
 package org.acme.service;
 
-public class CarteVitaleServiceTest {
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.acme.controller.ImagesController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static io.restassured.RestAssured.given;
+
+
+@QuarkusTest
+class CarteVitaleServiceTest {
+
+    @Test
+    public void testVerifierEndpointValide() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"carteVitale\":\"1234\"}")
+                .when().post("http://localhost:8081/carteVitale")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testVerifierEndpointInvalide() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"carteVitale\":\"test\"}")
+                .when().post("http://localhost:8081/carteVitale")
+                .then()
+                .statusCode(400);
+    }
+
 }
