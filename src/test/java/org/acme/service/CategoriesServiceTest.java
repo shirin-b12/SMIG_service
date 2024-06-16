bb5d06c9f35eda56dec27db2126aedd57c1166c1
Imran C. <Imranchk17@gmail.com>
2024-06-16 21:33:50 +0200

    🚧 Test : En cours... de construction de "CategoriesServiceTest.java".

--- src/test/java/org/acme/service/CategoriesServiceTest.java
+++ src/test/java/org/acme/service/CategoriesServiceTest.java
@@ -1,4 +1,39 @@
 package org.acme.service;
 
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class CategoriesServiceTest {
+
+    @Test
+    public void testGetCategories() {
+        given()
+                .contentType(ContentType.JSON)
+                .when().get("http://localhost:8081/categories")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testCreateCategory() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"nom_cat\":\"technologie\"}")
+                .when().post("http://localhost:8081/categories")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testCreateCategoryFailed() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"nom\":\"technologie\"}")
+                .when().post("http://localhost:8081/categories")
+                .then()
+                .statusCode(400);
+    }
+
 }
